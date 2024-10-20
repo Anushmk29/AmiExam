7d538f4698d3816c7041171075cb6048054b1239
Anush MK <101824229+Anushmk29@users.noreply.github.com>
2024-10-20 19:49:11 +0530

    Update userModel.js

--- AmiExam-Backend/Models/userModel.js
+++ AmiExam-Backend/Models/userModel.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    username: { type: String, required: true,unique:true },
+    year:{type:Number},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["student", "admin"], required: true },
+    role: { type: String, enum: ["student", "admin"] },
     progress: [
       {
         examId: { type: mongoose.Schema.Types.ObjectId, ref: "Exam" },
